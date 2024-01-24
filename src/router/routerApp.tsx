f92d4e3ec699d8f6f3f1f4e160ff1c821e6cc888
Alexandr Gusletsov <chegeadaun@yandex.ru>
2024-01-24 14:33:06 +0300

    refactor routerApp file

--- src/router/routerApp.tsx
+++ src/router/routerApp.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import { memo } from 'react';
 import { ROUTES } from './routerPaths';
-import UserList from '~/app/user-list';
-import PageNotFound from '~/components/page-not-found';
-import { SignUp } from '~/app/sign-up';
 import { RequireAuth } from '~/hoc/RequireAuth';
+import UserList from '~/app/user-list';
 import UserAbout from '~/app/user-about';
+import { SignUp } from '~/app/sign-up';
+import PageNotFound from '~/components/page-not-found';
 
 function RouterApp() {
   return (
